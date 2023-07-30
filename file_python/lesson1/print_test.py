616708d2f8959572eedbbe44b329a56adfd28a24
cclois <cclois@hotmail.com>
2023-07-30 21:17:37 +1000

    Add print test

---
+++ file_python/lesson1/print_test.py
@@ -0,0 +1,15 @@
+# Lesson 1: Print function
+
+## 1.1 Print int
+print(1)
+
+## 1.2 Print str
+print("hello world")
+
+## 1.3 Print str with quotation mark
+print("I'm CCLois") # 1st method
+print('I\'m CClois') # 2nd method, use back slash to reverse the quotation effect
+
+## 1.4 Print str with "+"
+print ('Pen' + 'Pineapple')
+
