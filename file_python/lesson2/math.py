ca70719394961306b130832567a27aab57a36213
cclois <cclois@hotmail.com>
2023-07-31 21:05:05 +1000

    Create math.py and update the python file

---
+++ file_python/lesson2/math.py
@@ -0,0 +1,18 @@
+# Lesson 2: Math
+
+## 1.1 + - * / % //
+
+print(1 + 1)
+print(3 - 1)
+
+## how to calclulate power
+print(2**2)
+
+## how to calculate residue
+print(8%2)
+print(9%2)
+
+## how to calculate rounding
+print(99//2)
+print(9//4)
+
