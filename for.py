a085df4b890a8c6923cd2b3e1fec58297d14c7a5
cclois <cclois@hotmail.com>
2023-07-31 21:35:42 +1000

    for.py update how to use step

--- for.py
+++ for.py
@@ -1,8 +1,16 @@
 
 
 ## exercise 1
-lst = [1,2,3,4,5,6,7,8,9]
+lst = [1,2,3,4,5,6,7,8,9,10,11,12]
 
 for i in lst:
     print(i)
+    print('this is a text')
     
+## exercise 2
+for i in range(1,10):
+    print(i)
+    
+## exercise 3
+for i in range(1,10,2):  # 2 is step
+    print(i)
